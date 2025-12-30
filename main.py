304dca7e66022ed2cac5a02b22798d438a921ddb
ntolbert <racam9W@gmail.com>
2025-12-30 13:35:20 -0500

    added game logic

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    while True:
+    updateable.group=()
+    drawable.group = ()
+    Player.containers = (updateable,drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+       while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+        screen.fill("black") 
+        player.draw(screen) 
         pygame.display.flip()
-    
-        print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}") 
-
+       
+        #print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+        #print(f"Screen width: {SCREEN_WIDTH}")
+        #print(f"Screen height: {SCREEN_HEIGHT}") 
+        player.update(dt)
         dt += clock.tick(60)/1000
         print(dt)
 if __name__ == "__main__":
